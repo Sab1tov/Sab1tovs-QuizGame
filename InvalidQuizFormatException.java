a87f64e669e9b941150cf12355d075a57c5b97b5
Sab1tov <83912155+Sab1tov@users.noreply.github.com>
2022-05-15 14:52:20 +0600

    Add files via upload

---
+++ InvalidQuizFormatException.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+class InvalidQuizFormatException extends Exception {
+    public InvalidQuizFormatException(){
+        super("Exception in thread \"main\" InvalidQuizFormat");
+    }
+}
---
+++ Question.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public abstract class Question {
+    private String description;
+    private String answer;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
