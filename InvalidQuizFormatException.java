b700aae78cd9fe04c0e1e5ede65caa4b88d4a680
Yasin <83912155+Sab1tov@users.noreply.github.com>
2022-05-15 15:33:46 +0600

    Delete InvalidQuizFormatException.java

--- InvalidQuizFormatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-class InvalidQuizFormatException extends Exception {
-    public InvalidQuizFormatException(){
-        super("Exception in thread \"main\" InvalidQuizFormat");
-    }
-}
